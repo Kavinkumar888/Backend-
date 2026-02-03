cff828d2f23dddd86e13c4f7d32526d39807c663
Kavinkumar888 <kavi8668182885@gmail.com>
2026-02-03 11:56:50 +0530

    jj

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product.model");
+const Product = require("../../models/product.model");
 
 /* -------- GET PRODUCTS (FAST LIST) -------- */
 /* -------- GET PRODUCTS (REAL PAGINATION) -------- */

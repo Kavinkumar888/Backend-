c00d5f682694f276a8b24e3ce6d03961673710a9
Kavinkumar888 <kavi8668182885@gmail.com>
2026-02-03 12:48:49 +0530

    jj

--- server.js
+++ server.js
@@ -1,37 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
 require("dotenv").config();
+const app = require("./src/app");
 
-const connectDB = require("./src/config/db");
-const productRoutes = require("./src/routes/product.routes");
-
-const app = express();
-
-/* DB */
-connectDB();
-
-/* MIDDLEWARE */
-app.use(cors({
-  origin: ["https://sssventures.in", "http://localhost:3000"],
-}));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-/* STATIC */
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-/* ROUTES */
-app.use("/api/products", productRoutes);
-
-/* TEST */
-app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
-});
-
-/* START */
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });

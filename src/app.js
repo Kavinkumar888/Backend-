8e87a96469713bb5a7168044363d4b309bd2dc89
Kavinkumar888 <kavi8668182885@gmail.com>
2026-02-04 18:37:45 +0530

    it

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(
 /* ✅ STATIC UPLOADS — CORRECT */
 app.use(
   "/uploads",
-  express.static(path.join(__dirname, "src/uploads"))
+  express.static(path.join(__dirname, "uploads"))
 );
 
 
